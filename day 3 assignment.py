0a5b2cf2507d0166a63a4765d214b7ea03ed4fbe
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-14 20:20:23 +0530

    Add files via upload

---
+++ day 3 assignment.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import ttk
+
+window = Tk()
+
+window.title("Registration Screen")
+
+window.geometry('300x300')
+
+window.configure(background = "green");
+
+Firstname = Label(window ,text = "First Name").grid(row = 0,column = 0)
+LastName = Label(window ,text = "Last Name").grid(row = 1,column = 0)
+Email = Label(window ,text = "Email Id").grid(row = 2,column = 0)
+Mobile = Label(window ,text = "Contact Number").grid(row = 3,column = 0)
+
+Firstname1 = Entry(window).grid(row = 0,column = 1)
+Lastname1 = Entry(window).grid(row = 1,column = 1)
+Email1 = Entry(window).grid(row = 2,column = 1)
+Mobile1 = Entry(window).grid(row = 3,column = 1)
+
+
+def clicked():
+            res = "Welcome to " + txt.get()
+            lbl.configure(text= res)
+btn = ttk.Button(window ,text="Submit").grid(row=4,column=0)
+window.mainloop()
