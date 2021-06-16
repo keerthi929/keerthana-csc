f36e60a9d1fc9088773b0d2a136437fc533674bf
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-16 19:08:48 +0530

    Add files via upload

---
+++ task 5.py
@@ -0,0 +1,25 @@
+#1)Task 5
+
+lst=[5,10,15,20,30,35,37,40,50]
+lst1=lst.insert(4,25)
+print(lst)
+lst2=lst.pop(7)
+print(lst)
+a=max(lst)
+print(a ,"is maximum")
+b=min(lst)
+print(b,"is minimum")
+
+
+
+
+
+          OUTPUT
+       
+[5, 10, 15, 20, 25, 30, 35, 37, 40, 50]
+
+[5, 10, 15, 20, 25, 30, 35, 40, 50]
+
+50 is maximum
+
+5 is minimum
