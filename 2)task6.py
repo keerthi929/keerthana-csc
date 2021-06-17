054167be8e6785d867dd9f339b8fa875738fa2c9
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-17 20:04:47 +0530

    Add files via upload

---
+++ 2)task6.py
@@ -0,0 +1,17 @@
+   #remove key from dictionary
+
+
+dict={1:'sai',2:'ram',3:'sam',4:'goat'}
+print(dict)
+d1=dict.pop(3)
+print(dict)
+print('removed element:',d1)
+
+
+           Output
+
+{1: 'sai', 2: 'ram', 3: 'sam', 4: 'goat'}
+{1: 'sai', 2: 'ram', 4: 'goat'}
+removed element: sam
+
+[Program finished]
