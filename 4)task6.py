8acdfd496075257ac930182492b7fe1df3bd2741
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-17 20:28:39 +0530

    Add files via upload

---
+++ 4)task6.py
@@ -0,0 +1,12 @@
+#to find length of the set
+
+
+s={"English","Greeks","Spanish","Tamil"}
+n=len(s)
+print("The length of the set s is:",n)
+
+     Output
+
+The length of the set s is: 4
+
+[Program finished]
