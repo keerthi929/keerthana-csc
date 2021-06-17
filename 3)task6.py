d7a05bb768838426c202f812ed19e52b6bd7da1f
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-17 20:18:50 +0530

    Add files via upload

---
+++ 3)task6.py
@@ -0,0 +1,16 @@
+
+#to map two list into a dictionary
+
+
+countries=["USA","India","Germany","France"]
+cities=["Washington","Newdelhi","Berlin","Paris"]
+z=zip(countries,cities)
+d=dict(z)
+print(d)
+
+     Output
+
+{'USA': 'Washington', 'India': 'Newdelhi', 'Germany': 'Berlin', 'France': 'Paris'}
+
+[Program finished]
+
