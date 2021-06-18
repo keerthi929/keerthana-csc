ab7a47f948a9ba8aaccefe09e859232f08cfd37e
keerthi929 <69851175+keerthi929@users.noreply.github.com>
2021-06-18 20:02:38 +0530

    Add files via upload

---
+++ 1)task7.py
@@ -0,0 +1,40 @@
+
+#Addition
+
+def sum(a,b):
+	c=a+b
+	return c
+num1=int(input("enter first number:"))
+num2=int(input("enter second number"))
+print("Addition of two numbers is ",+sum(num1,num2))
+
+
+#subtraction
+
+def sub(a,b):
+	c=a-b
+	return c
+num3=int(input("enter first number:"))
+num4=int(input("enter second number:"))
+print("Subtraction of two numbers is ",+sub(num3,num4))
+
+#multiplication 
+
+def mul(a,b):
+	c=a*b
+	return c
+num4=int(input("enter first number:"))
+num5=int(input("enter second number"))
+print("multiplication of two numbers is ",+mul(num4,num5))
+
+
+#division
+
+def divi(a,b):
+	c=a/b
+	return c
+num6=int(input("enter first number:"))
+num7=int(input("enter second number"))
+print("Division of two numbers is ",+divi(num6,num7))
+
+
---
+++ task 7 output .py
@@ -0,0 +1,21 @@
+
+             #task7 Output
+
+
+enter first number:10
+enter second number20
+Addition of two numbers is  30
+
+enter first number:50
+enter second number:25
+Subtraction of two numbers is  25
+
+enter first number:25
+enter second number25
+multiplication of two numbers is  625
+
+enter first number:100
+enter second number:5
+Division of two numbers is 20
+
+[Program finished]
